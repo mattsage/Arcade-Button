ba18c6fdf0f8d187aeebaf45563b1d9610a77c0b
Matthew Sage <mattsage@users.noreply.github.com>
2016-02-26 14:20:52 +0000

    Update ArcadeMenu.py
    Added power off switch

--- ArcadeMenu.py
+++ ArcadeMenu.py
@@ -3,6 +3,7 @@ import os
 
 button = Button(17)
 button2 = Button(18)
+button3 = Button(27)
 
 while True:
         if button.is_pressed:
@@ -12,3 +13,9 @@ while True:
         if button2.is_pressed:
                 os.system("startx")
                 exit()
+                
+        if button3.is_pressed:
+                os.system("poweroff")
+                exit()
+
+#Need another button for Exit game??
