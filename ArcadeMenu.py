779dd9848ca578b490c4635b1e2812944b218faf
Matthew Sage <mattsage@users.noreply.github.com>
2016-03-04 13:40:36 +0000

    Update ArcadeMenu.py
    Commented out commands and added "echo's" for debugging purposes

--- ArcadeMenu.py
+++ ArcadeMenu.py
@@ -7,15 +7,18 @@ button3 = Button(27)
 
 while True:
         if button.is_pressed:
-                os.system("emulationstation")
-                exit()
+                os.system("echo emulationstation")
+                #os.system("emulationstation")
+                #exit()
 
         if button2.is_pressed:
-                os.system("startx")
-                exit()
+                os.system("echo startx")
+                #os.system("startx")
+                #exit()
                 
         if button3.is_pressed:
-                os.system("poweroff")
-                exit()
+                os.system("echo poweroff")
+                #os.system("poweroff")
+                #exit()
 
-#Need another button for Exit game??
+#Need another button for Exit game?
