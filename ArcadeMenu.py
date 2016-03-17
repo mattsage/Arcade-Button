9d36c6b5efc5d60f11609ccd47a74ab0286b481f
mattsage@rocketmail.com <mattsage@rocketmail.com>
2016-03-17 20:18:08 +0000

    Button Hat Complete

--- ArcadeMenu.py
+++ ArcadeMenu.py
@@ -1,24 +1,24 @@
 from gpiozero import Button
 import os
 
-button = Button(17)
-button2 = Button(18)
+button = Button(3)
+button2 = Button(5)
 button3 = Button(27)
 
 while True:
         if button.is_pressed:
-                os.system("echo emulationstation")
-                #os.system("emulationstation")
-                #exit()
+                #os.system("echo emulationstation")
+                os.system("emulationstation")
+                exit()
 
         if button2.is_pressed:
-                os.system("echo startx")
-                #os.system("startx")
-                #exit()
+                #os.system("echo startx")
+                os.system("startx")
+                exit()
                 
         if button3.is_pressed:
-                os.system("echo poweroff")
-                #os.system("poweroff")
-                #exit()
+                #os.system("echo poweroff")
+                os.system("sudo poweroff")
+                exit()
 
 #Need another button for Exit game?
